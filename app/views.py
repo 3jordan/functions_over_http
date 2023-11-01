d267d45adbc74bf1595d41190d91498656fbdc7d
Jordan <jdonahue24@basecampcodingacademt.org>
2023-11-01 10:23:46 -0500

    Final Type Annotations

--- app/views.py
+++ app/views.py
@@ -5,15 +5,17 @@ from django.http.request import HttpRequest
 # Create your views here.
 
 
-def hey_you(request: HttpRequest, name) -> HttpResponse:
+def hey_you(request: HttpRequest, name: str) -> HttpResponse:
     return HttpResponse(f"Hey, {name}!")
 
 
-def how_old(request: HttpRequest, end, birthyear) -> HttpResponse:
+def how_old(request: HttpRequest, end: int, birthyear: int) -> HttpResponse:
     age_in_end_year = end - birthyear
     return HttpResponse(age_in_end_year)
 
 
-def can_i_take_your_order(request: HttpRequest, burgers, fries, drinks) -> HttpResponse:
+def can_i_take_your_order(
+    request: HttpRequest, burgers: int, fries: int, drinks: int
+) -> HttpResponse:
     total = (burgers * 4.50) + (fries * 1.50) + (drinks * 1.00)
     return HttpResponse(f"${total}")
